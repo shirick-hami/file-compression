2997b5721086a7e9ac10eadeb4be677ac2a2145f
Saptarick mishra <spmishra@lentra.ai>
2025-12-30 18:04:39 +0530

    initial commit

---
+++ src/main/java/com/rickm/filecompression/FileCompressionApplication.java
@@ -0,0 +1,13 @@
+package com.rickm.filecompression;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileCompressionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileCompressionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rickm/filecompression/FileCompressionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rickm.filecompression;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileCompressionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
